a5141bc4c86bedd956773a1eb4a78eb3c73c38cb
ST10460086 <166159899+Reece52SRT@users.noreply.github.com>
2025-03-13 18:18:11 +0200

    Add project files.

---
+++ ICE_Task_3/Shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_Task_3
+{
+    public abstract class Shape
+    {
+        protected string Name;
+
+        public Shape (string name)
+        {
+            this.Name = name;
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"Virtual Display");
+        }
+    }
+}
