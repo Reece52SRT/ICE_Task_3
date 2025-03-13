38586bcd419c00509346fed0182844514d1be67a
ST10460086 <166159899+Reece52SRT@users.noreply.github.com>
2025-03-13 18:48:59 +0200

    Third Commit - Circle Class Added

---
+++ ICE_Task_3/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_Task_3
+{
+    internal class Circle : Shape, ICalculateArea
+    {
+        public double Radius { get; set; }
+        public Circle(string name, double radius) : base(name)
+        {
+            this.Radius = radius;
+        }
+
+        public void ICalculateArea()
+        {
+            double circleArea = Math.PI * Math.Pow(Radius, 2);
+            Console.WriteLine($"The total area is: {circleArea}");
+        }
+    }
+}
